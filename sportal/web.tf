0c06f634cf8650ac9a4168ae5f6db38b10dab575
Trish <trishul.shah@performgroup.com>
2019-05-16 11:23:17 +0100

    add new sportal web ami and increase az-a instances to 2

--- sportal/web.tf
+++ sportal/web.tf
@@ -1,5 +1,5 @@
 variable "num_instances_web_aza" {
-  default = 1
+  default = 2
 }
 
 variable "num_instances_web_azb" {
@@ -11,7 +11,7 @@ variable "num_instances_web_azc" {
 }
 
 resource "aws_instance" "csportal-web-aza" {
-  ami                         = "ami-0ea2ce632f8f3f1e4"
+  ami                         = "ami-0abc3a165de8eca35"
   key_name                    = "${var.keys["${terraform.workspace}"]}"
   instance_type               = "t3.medium"
   vpc_security_group_ids      = ["${aws_security_group.sportal_web.id}"]
