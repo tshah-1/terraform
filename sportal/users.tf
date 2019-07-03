640d53aa881f50d3880efb49e6f37ce6cdb5e5b0
Trish <trishul.shah@performgroup.com>
2019-07-03 14:53:22 +0100

    add ansible user with ec2-readonly

--- sportal/users.tf
+++ sportal/users.tf
@@ -48,13 +48,24 @@ resource "aws_iam_user" "sebastianbugajny" {
   path = "/"
 }
 
+resource "aws_iam_user" "ansible" {
+  name = "ansible"
+  path = "/"
+}
+
 resource "aws_iam_group" "admin-users" {
   name = "admin-users"
   path = "/"
 }
 
+resource "aws_iam_group" "ec2-readonly-users" {
+  name = "ec2-readonly-users"
+  path = "/"
+}
+
 resource "aws_iam_group_membership" "admin-user-membership" {
   name = "admin-user-membership"
+
 
   users = [
     "${aws_iam_user.gernotheschl.name}",
@@ -72,6 +83,16 @@ resource "aws_iam_group_membership" "admin-user-membership" {
   group = "${aws_iam_group.admin-users.name}"
 }
 
+resource "aws_iam_group_membership" "ec2-readonly-user-membership" {
+  name = "ec2-readonly-user-membership"
+
+users = [
+    "${aws_iam_user.ansible.name}",
+  ]
+
+  group = "${aws_iam_group.ec2-readonly-users.name}"
+}
+
 resource "aws_iam_group_policy" "explicit-admin" {
   name  = "explicit-admin"
   group = "${aws_iam_group.admin-users.id}"
@@ -86,6 +107,23 @@ resource "aws_iam_group_policy" "explicit-admin" {
       "Resource": "*"
     }
   ]
+}
+EOF
+}
+
+resource "aws_iam_group_policy" "ec2-readonly" {
+  name  = "ec2-readonly"
+  group = "${aws_iam_group.ec2-readonly-users.id}"
+
+  policy = <<EOF
+{
+   "Version": "2012-10-17",
+   "Statement": [{
+      "Effect": "Allow",
+      "Action": "ec2:Describe*",
+      "Resource": "*"
+    }
+   ]
 }
 EOF
 }
